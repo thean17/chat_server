96f3b2286c155751fc182cedddc2bd806ee43bf6
thean17 <59770595+thean17@users.noreply.github.com>
2021-05-12 21:50:58 +0800

    update minio endPoint

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ const Configuration = {
 		dbName: 'game_app_chat',
 	},
 	minio: {
-		endPoint: 'http://127.0.0.1',
+		endPoint: '127.0.0.1',
 		port: 9000,
 		accessKey: 'minioadmin',
 		secretKey: 'minioadmin',
--- minio/index.js
+++ minio/index.js
@@ -1,15 +1,15 @@
-import Minio from 'minio';
+import { Client } from 'minio';
 import Configuration from '../config';
 
 /**
- * @type {Minio.Client}
+ * @type {Client}
  */
 let minio;
 
 export const connectMinio = () => {
 	if (!!minio) return minio;
 
-	minio = new Minio.Client({
+	minio = new Client({
 		endPoint: Configuration.minio.endPoint,
 		port: Configuration.minio.port,
 		accessKey: Configuration.minio.accessKey,
