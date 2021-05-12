19258c036e60fd8dc36b4a8887657fd08e413603
thean17 <59770595+thean17@users.noreply.github.com>
2021-05-12 21:40:04 +0800

    add chat socket handler

---
+++ socket/chat.js
@@ -0,0 +1,150 @@
+import Message, { MessageType } from '../database/model/message';
+import Player from '../database/model/user';
+import Room from '../database/model/room';
+import RoomMember from '../database/model/roomMember';
+import fs from 'fs';
+import FileType from 'file-type';
+
+/**
+ *
+ * @param {import('http').Server} server
+ * @returns
+ */
+export default function (server) {
+	/**
+	 * @type {import('socket.io').Server}
+	 */
+	const io = require('socket.io')(server, {
+		cors: {
+			origin: '*',
+		},
+	});
+
+	const users = {};
+
+	/**
+	 *
+	 * @param {import('socket.io').Socket} socket
+	 */
+	const handleConnection = (socket) => {
+		console.log('new connection');
+
+		users[socket.id] = { username: socket.id, connectedAt: new Date() };
+
+		socket.on('join_room', async (id) => {
+			console.log('join_room: ', id);
+
+			const [room, player] = await Promise.all([
+				Room.findOne({ uid: id }),
+				Player.findOne({ playerName: 'thean1' }),
+			]);
+			const roomMember = await RoomMember.findOne({
+				room: room._id,
+				// player: socket.handshake.headers.id,
+				player: player._id,
+			});
+
+			console.log({
+				room,
+				player,
+				roomMember,
+			});
+
+			if (roomMember) {
+				socket.join(room.uid);
+
+				// socket.emit('receive_message_from_room', 'Test', { something: 'Something' });
+
+				// socket
+				// 	.to(room.uid)
+				// 	.emit('receive_message_from_room', 'Test', { something: 'Something' });
+			} else {
+				socket.emit('join_room', null);
+			}
+		});
+
+		socket.on(
+			'send_message_to_room',
+			async (id, message, type = MessageType.Text) => {
+				if (socket.rooms.has(id)) {
+					console.log(socket.handshake.headers);
+
+					console.log({
+						message,
+						type,
+					});
+					if (type === MessageType.Image) {
+						const file = Buffer.from(message);
+						const fileType = await FileType.fromBuffer(file);
+					}
+
+					const [room, player] = await Promise.all([
+						Room.findOne({ uid: id }),
+						Player.findOne({ playerName: socket.handshake.headers.playername }),
+					]);
+					const msg = await Message.create({
+						player: player._id,
+						playerId: player.playerId,
+						playerName: player.playerName,
+						room: room._id,
+						roomId: room.roomId,
+						type,
+						content: type === MessageType.Image ? 'image' : message,
+						createdBy: player.playerId,
+					}).then((doc) => ({
+						id: doc.uid,
+						message: doc.content,
+						dateTime: doc.createdAt,
+						sender: {
+							Id: player.playerName,
+							UserName: player.playerName,
+							Name: player.playerName,
+							AvatarFileName:
+								'https://www.thewrap.com/wp-content/uploads/2021/03/Invincible.jpeg',
+							IsBlackList: false,
+							IsFriend: false,
+						},
+					}));
+
+					socket.emit('receive_message_from_room', msg, type);
+					socket.to(id).emit('receive_message_from_room', msg, type);
+				}
+			}
+		);
+
+		socket.on('disconnect', async () => {
+			delete users[socket.id];
+
+			// if (socket.roomId) {
+			// 	const matchingSockets = await io.in(socket.roomId).allSockets();
+			// 	const isDisconnected = matchingSockets.size === 0;
+			// 	if (isDisconnected) {
+			// 		// notify other users
+			// 		socket.broadcast.emit('user disconnected', socket.userID);
+			// 		// update the connection status of the session
+			// 		sessionStore.saveSession(socket.sessionID, {
+			// 			userID: socket.userID,
+			// 			username: socket.username,
+			// 			connected: false,
+			// 		});
+			// 	}
+			// }
+		});
+
+		socket.on('set_typing', () => {});
+
+		socket.on('unset_typing', () => {});
+
+		socket.on('get_active_users', () => {});
+	};
+
+	io.on('connection', handleConnection);
+
+	io.on('disconnect', (reason) => {
+		console.log('disconnected: ', reason);
+
+		delete users[socket.id];
+	});
+
+	return io;
+}
